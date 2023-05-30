4b9511257106a9863a5653184ed2071bad1da0ef
Pascal Garcia Esteves <58555606+PascalEsteves@users.noreply.github.com>
2023-05-30 23:15:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 f = open(os.path.join(__location__, 'config.json'))
 config = Valid_Blob_Config(json.load(f))
 
-
 class Blob_Connection():
   def __init__(self):
     self.connection = Connection(config)
