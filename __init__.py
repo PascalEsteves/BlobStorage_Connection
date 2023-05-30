f373be69da532d59105150821532de29d88c3574
Pascal Garcia Esteves <58555606+PascalEsteves@users.noreply.github.com>
2023-05-30 10:03:31 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import os
+import json
+from .config import Valid_Blob_Config
+from .blob_connection import Connection
+
+# f = open('config.json')
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+f = open(os.path.join(__location__, 'config.json'))
+config = Valid_Blob_Config(json.load(f))
+
+
+class Blob_Connection():
+  def __init__(self):
+    self.connection = Connection(config)
+
